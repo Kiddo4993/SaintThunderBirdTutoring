cfdda7d70f9ae3d6982db111bc732681377e9826
Dylan <dylanduancanada@gmail.com>
2025-11-28 12:00:17 -0800

    final

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const User = require('./saint-thunderbird-backend/models/User');  // ← ADD THIS LINE
-const authRoutes = require('./saint-thunderbird-backend/routes/auth');
-const tutorRoutes = require('./saint-thunderbird-backend/routes/tutor');
+const User = require('./models/User');
+const authRoutes = require('./routes/auth');
+const tutorRoutes = require('./routes/tutor');
 
 const app = express();
 // ... rest of code
