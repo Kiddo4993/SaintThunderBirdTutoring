db36b04a77fff4310fc43fb2d2b49b2b3a1a8448
Dylan <dylanduancanada@gmail.com>
2025-11-28 19:57:14 -0800

    thanks

--- server.js
+++ server.js
@@ -31,3 +31,9 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
